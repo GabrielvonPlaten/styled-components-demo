ab0a4903ae51cb8ae9ff1c9006b601a0ddcb0da4
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-07-05 21:54:00 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: path.join(__dirname, 'public', 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{ 
+      loader: 'babel-loader', 
+      test: /\.jsx?$/,
+      exclude: /node_modules/ 
+    },
+    {
+      test: /\.s?a?c?ss$/,
+      use: [
+        MiniCssExtractPlugin.loader, 
+        'css-loader', 
+        'sass-loader'
+      ] // multiple loaders using an array
+    },
+    {
+      test: /\.(png|jpg|gif|svg)$/i,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: undefined,
+          },
+        },
+      ],
+    },
+    ]
+  },
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true,
+    publicPath: '/dist/'
+  },
+  mode: 'development',
+  plugins: [
+    new MiniCssExtractPlugin({ 
+      filename: "style.css"
+    }),
+    new Dotenv()
+  ], 
+};
